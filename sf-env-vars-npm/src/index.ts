d34373ad7cd8ba080edba71ca45f9d6c85bebebb
Charlie Jonas <charlie@callawaycloudconsulting.com>
2020-03-25 13:47:59 -0600

    supporting any type on NPM package

--- sf-env-vars-npm/src/index.ts
+++ sf-env-vars-npm/src/index.ts
@@ -1,16 +1,16 @@
-import {EnvVarRecord} from './generated';
+import { EnvVarRecord } from './generated';
 
-type EnvValType = number | string | boolean | string[] | {[key: string]: string};
+type EnvValType = number | string | boolean | string[] | { [key: string]: string };
 
 export const load = async <T>(varsObject: { new(): T }): Promise<T> => {
 
-  let result = new varsObject();
+  const result = new varsObject();
   const envVars = await EnvVarRecord.retrieve((fields) => (
     {
       select: [...fields.select('datatype', 'developerName', 'value')],
       where: [
-        {field: fields.select('developerName'), val: Object.keys(result)}
-      ]
+        { field: fields.select('developerName'), val: Object.keys(result) },
+      ],
     }
   ));
 
@@ -18,23 +18,24 @@ export const load = async <T>(varsObject: { new(): T }): Promise<T> => {
     let val: EnvValType;
     switch (envVar.datatype.toLowerCase()) {
       case 'integer':
-      val = parseInt(envVar.value);
-      break;
+        val = parseInt(envVar.value);
+        break;
       case 'decimal':
-      val = parseFloat(envVar.value);
-      break;
+        val = parseFloat(envVar.value);
+        break;
       case 'boolean':
-      val = envVar.value.toLowerCase() === 'true';
-      break;
+        val = envVar.value.toLowerCase() === 'true';
+        break;
       case 'string[]':
       case 'map<string,string>':
-      val = JSON.parse(envVar.value);
-      break;
+      case 'any':
+        val = JSON.parse(envVar.value);
+        break;
       default:
-      val = envVar.value;
+        val = envVar.value;
     }
     const key = envVar.developerName;
     result[key] = val;
   }
   return result;
-}
+};
